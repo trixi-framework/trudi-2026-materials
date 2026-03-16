9296d9d308d5983c46e4596d20d9d337ca9f29f6
Marco Artiano <artianomarco97@gmail.com>
2026-03-16 08:01:00 +0100

    update vector invariant

---
+++ HandsOn_TrixiAtmo.jl/3_vector_invariant/equations/compressible_euler_vectorinvariant_3d.jl
@@ -0,0 +1,301 @@
+using Trixi
+using Trixi: AbstractCompressibleEulerEquations, @muladd, norm
+import Trixi:
+    varnames,
+    cons2cons,
+    cons2prim,
+    cons2entropy,
+    entropy,
+    FluxLMARS,
+    boundary_condition_slip_wall,
+    flux,
+    max_abs_speeds,
+    max_abs_speed,
+    max_abs_speed_naive,
+    have_nonconservative_terms,
+    True, prim2cons, False
+# By default, Julia/LLVM does not use fused multiply-add operations (FMAs).
+# Since these FMAs can increase the performance of many numerical algorithms,
+# we need to opt-in explicitly.
+# See https://ranocha.de/blog/Optimizing_EC_Trixi for further details.
+@muladd begin
+#! format: noindent
+
+struct CompressibleEulerVectorInvariantEquations3D{RealT<:Real} <:
+       AbstractCompressibleEulerEquations{3,6}
+    p_0::RealT # reference pressure in Pa
+    c_p::RealT # specific heat at constant pressure in J/(kg K)
+    c_v::RealT # specific heat at constant volume in J/(kg K)
+    g::RealT # gravitational acceleration in m/s²
+    R::RealT # gas constant in J/(kg K)
+    gamma::RealT # ratio of specific heats 
+    inv_gamma_minus_one::RealT # = inv(gamma - 1); can be used to write slow divisions as fast multiplications
+    K::RealT # = p_0 * (R / p_0)^gamma; scaling factor between pressure and weighted potential temperature
+    stolarsky_factor::RealT # = (gamma - 1) / gamma; used in the stolarsky mean
+    function CompressibleEulerVectorInvariantEquations3D(; c_p, c_v, gravity)
+        c_p, c_v, g = promote(c_p, c_v, gravity)
+        p_0 = 100_000
+        R = c_p - c_v
+        gamma = c_p / c_v
+        inv_gamma_minus_one = inv(gamma - 1)
+        K = p_0 * (R / p_0)^gamma
+        stolarsky_factor = (gamma - 1) / gamma
+        return new{typeof(c_p)}(
+            p_0,
+            c_p,
+            c_v,
+            g,
+            R,
+            gamma,
+            inv_gamma_minus_one,
+            K,
+            stolarsky_factor,
+        )
+    end
+end
+
+function varnames(::typeof(cons2cons), ::CompressibleEulerVectorInvariantEquations3D)
+    ("rho", "v1", "v2", "v3", "rho_theta", "phi")
+end
+
+have_nonconservative_terms(::CompressibleEulerVectorInvariantEquations3D) = True()
+
+varnames(::typeof(cons2prim), ::CompressibleEulerVectorInvariantEquations3D) =
+    ("rho", "v1", "v2", "v3", "p", "phi")
+
+
+@inline function Trixi.boundary_condition_slip_wall(
+    u_inner,
+    normal_direction::AbstractVector,
+    x,
+    t,
+    surface_flux_function,
+    equations::CompressibleEulerVectorInvariantEquations3D
+)
+
+    Trixi.boundary_condition_slip_wall(u_inner, normal_direction, x, t, surface_flux_function, equations, Trixi.combine_conservative_and_nonconservative_fluxes(surface_flux_function, equations))
+end    
+
+@inline function Trixi.boundary_condition_slip_wall(
+    u_inner,
+    normal_direction::AbstractVector,
+    x,
+    t,
+    surface_flux_function,
+    equations::CompressibleEulerVectorInvariantEquations3D, combine_conservative_and_nonconservative_fluxes::True
+)
+
+    # normalize the outward pointing direction
+    normal = normal_direction / norm(normal_direction)
+
+    # compute the normal velocity
+    u_normal = normal[1] * u_inner[2] + normal[2] * u_inner[3] + normal[3] * u_inner[4]
+
+    # create the "external" boundary solution state
+    u_boundary = SVector(
+        u_inner[1],
+        u_inner[2] - 2 * u_normal * normal[1],
+        u_inner[3] - 2 * u_normal * normal[2],
+        u_inner[4] - 2 * u_normal * normal[3],
+        u_inner[5],
+	u_inner[6]
+    )
+
+    #   calculate the boundary flux
+    flux, _ = surface_flux_function(u_inner, u_boundary, normal_direction, equations)
+    return flux
+end
+
+@inline function Trixi.boundary_condition_slip_wall(
+    u_inner,
+    normal_direction::AbstractVector,
+    x,
+    t,
+    surface_flux_functions,
+    equations::CompressibleEulerVectorInvariantEquations3D, combine_conservative_and_nonconservative_fluxes::False
+)
+    surface_flux_function, nonconservative_flux_function = surface_flux_functions
+    # normalize the outward pointing direction
+    normal = normal_direction / norm(normal_direction)
+
+    # compute the normal velocity
+    u_normal = normal[1] * u_inner[2] + normal[2] * u_inner[3] + normal[3] * u_inner[4]
+
+    # create the "external" boundary solution state
+    u_boundary = SVector(
+        u_inner[1],
+        u_inner[2] - 2 * u_normal * normal[1],
+        u_inner[3] - 2 * u_normal * normal[2],
+        u_inner[4] - 2 * u_normal * normal[3],
+        u_inner[5],
+	u_inner[6]
+    )
+
+    #   calculate the boundary flux
+    flux = surface_flux_function(u_inner, u_boundary, normal_direction, equations)
+    noncons_flux = nonconservative_flux_function(u_inner, u_boundary, normal_direction, equations)
+
+    return flux, noncons_flux
+end
+
+@inline function flux_surface_combined(
+    u_ll,
+    u_rr,
+    normal_direction::AbstractVector,
+    equations::CompressibleEulerVectorInvariantEquations3D)
+
+    rho_ll, v1_ll, v2_ll, v3_ll, rho_theta_ll = u_ll
+    rho_rr, v1_rr, v2_rr, v3_rr, rho_theta_rr = u_rr
+    _, _, _, _, exner_ll = cons2primexner(u_ll, equations)
+    _, _, _, _, exner_rr = cons2primexner(u_rr, equations)
+    theta_ll = rho_theta_ll / rho_ll
+    theta_rr = rho_theta_rr / rho_rr
+
+    v_dot_n_ll = v1_ll * normal_direction[1] + v2_ll * normal_direction[2] + v3_ll * normal_direction[3]
+    v_dot_n_rr = v1_rr * normal_direction[1] + v2_rr * normal_direction[2] + v3_rr * normal_direction[3]
+
+    # Average each factor of products in flux
+    rho_avg = 0.5f0 * (rho_ll + rho_rr)
+    theta_avg = 0.5f0 * (theta_ll + theta_rr)
+    v1_avg = 0.5f0 * (v1_ll + v1_rr)
+    v2_avg = 0.5f0 * (v2_ll + v2_rr)
+    v3_avg = 0.5f0 * (v3_ll + v3_rr)
+
+    jump_v1 = v1_rr - v1_ll
+    jump_v2 = v2_rr - v2_ll
+    jump_v3 = v3_rr - v3_ll
+
+    rho_v_ll = v1_ll * rho_ll * normal_direction[1] + v2_ll * rho_ll * normal_direction[2] + v3_ll * rho_ll * normal_direction[3]
+    rho_v_rr = v1_rr * rho_rr * normal_direction[1] + v2_rr * rho_rr * normal_direction[2] + v3_rr * rho_rr * normal_direction[3]
+
+    T_ll = theta_ll * exner_ll
+    T_rr = theta_rr * exner_rr
+
+    c_ll = sqrt(equations.gamma * equations.R * T_ll)
+    c_rr = sqrt(equations.gamma * equations.R * T_rr)
+
+    p_ll = exner_ll * rho_theta_ll * equations.R
+    p_rr = exner_rr * rho_theta_rr * equations.R
+    norm_ = norm(normal_direction)
+    c = 0.5f0 * (c_ll + c_rr)
+    c = 340.0
+    disst = - 1 / (2 * c) * (p_rr - p_ll) * norm_
+    c_adv = 0.5f0 * abs((v_dot_n_ll + v_dot_n_rr)) / norm(normal_direction)  
+    diss1 = 0.5f0 * c / rho_avg * (rho_v_rr - rho_v_ll) * normal_direction[1] / norm_
+    diss2 = 0.5f0 * c / rho_avg * (rho_v_rr - rho_v_ll) * normal_direction[2] / norm_
+    diss3 = 0.5f0 * c / rho_avg * (rho_v_rr - rho_v_ll) * normal_direction[3] / norm_
+    v_interface = 0.5f0 * (v_dot_n_ll + v_dot_n_rr) - 1 / (2 * c * rho_avg) * (p_rr - p_ll) * norm_
+
+    if (v_interface > 0)
+	f1, f2, f3, f4, f5 = u_ll * v_interface
+    else
+	f1, f2, f3, f4, f5 = u_rr * v_interface
+    end
+    f2 = - diss1 -0.5f0 * c_adv / rho_avg * (rho_rr * v1_rr - rho_ll * v1_ll) *norm(normal_direction) + v1_avg * disst
+    f3 = - diss2 -0.5f0 * c_adv / rho_avg * (rho_rr * v2_rr - rho_ll * v2_ll) *norm(normal_direction) + v2_avg * disst
+    f4 = - diss3 -0.5f0 * c_adv / rho_avg * (rho_rr * v3_rr - rho_ll * v3_ll) *norm(normal_direction) + v3_avg * disst
+
+    theta_grad_exner =  equations.c_p * theta_avg * (exner_rr - exner_ll)    
+	
+    advection = v1_avg * normal_direction[1] + v2_avg * normal_direction[2] + v3_avg * normal_direction[3] 
+    advection_x = advection * jump_v1
+    advection_y = advection * jump_v2
+    advection_z = advection * jump_v3
+    g2 = advection_x + theta_grad_exner * normal_direction[1]
+    g3 = advection_y + theta_grad_exner * normal_direction[2]
+    g4 = advection_z + theta_grad_exner * normal_direction[3]
+    return SVector(f1, f2 + 0.5f0 * g2, f3 + 0.5f0 * g3, f4 + 0.5f0 * g4, f5, zero(eltype(u_ll))), SVector(f1, f2 - 0.5f0 * g2, f3 - 0.5f0 * g3, f4 - 0.5f0 * g4, f5, zero(eltype(u_ll)))
+end
+
+@inline Trixi.combine_conservative_and_nonconservative_fluxes(::typeof(flux_surface_combined), equations::CompressibleEulerVectorInvariantEquations3D) = Trixi.True()
+
+@inline function max_abs_speed(
+    u_ll,
+    u_rr,
+    orientation::Integer,
+    equations::CompressibleEulerVectorInvariantEquations3D,
+)
+    rho_ll, v1_ll, v2_ll, v3_ll, p_ll = cons2prim(u_ll, equations)
+    rho_rr, v1_rr, v2_rr, v3_rr, p_rr = cons2prim(u_rr, equations)
+
+    # Get the velocity value in the appropriate direction
+    if orientation == 1
+        v_ll = v1_ll
+        v_rr = v1_rr
+    elseif orientation == 2
+        v_ll = v2_ll
+        v_rr = v2_rr
+    else
+	v_ll = v3_ll
+	v_rr = v3_rr
+    end
+    # Calculate sound speeds
+    c_ll = sqrt(equations.gamma * p_ll / rho_ll)
+    c_rr = sqrt(equations.gamma * p_rr / rho_rr)
+
+    return max(abs(v_ll) + c_ll, abs(v_rr) + c_rr)
+end
+
+# Convert conservative variables to primitive
+@inline function cons2prim(u, equations::CompressibleEulerVectorInvariantEquations3D)
+    rho, v1, v2, v3, rho_theta, phi = u
+
+    p = equations.K * rho_theta^equations.gamma
+
+    return SVector(rho, v1, v2, v3, p, phi)
+end
+
+# Convert primitive to conservative variables
+@inline function prim2cons(prim, equations::CompressibleEulerVectorInvariantEquations3D)
+    rho, v1, v2, v3, p, phi = prim
+rho_theta = (p / equations.p_0)^(1 / equations.gamma) * equations.p_0 / equations.R
+    return SVector(rho, v1, v2, v3, rho_theta, phi)
+end
+
+@inline function density(u, equations::CompressibleEulerVectorInvariantEquations3D)
+    rho = u[1]
+    return rho
+end
+
+@inline function velocity(u, equations::CompressibleEulerVectorInvariantEquations3D)
+    v1 = u[2]
+    v2 = u[3]
+    v3 = u[4]
+    return SVector(v1, v2, v3)
+end
+
+@inline function pressure(u, equations::CompressibleEulerVectorInvariantEquations3D)
+    rho, v1, v2, v3, rho_theta = u
+    p = equations.K * rho_theta^equations.gamma
+    return p
+end
+
+@inline function cons2primexner(
+    u,
+    equations::CompressibleEulerVectorInvariantEquations3D,
+)
+
+    rho, v1, v2, v3, rho_theta, phi = u
+
+    exner = (rho_theta * equations.R / equations.p_0)^(equations.R / equations.c_v)
+    return SVector(rho, v1, v2, v3, exner, phi)
+end
+
+@inline function exner_pressure(
+    u,
+    equations::CompressibleEulerVectorInvariantEquations3D,
+)
+
+    _, _, _, _, rho_theta = u
+
+    exner = (rho_theta * equations.R / equations.p_0)^(equations.R / equations.c_v)
+    return exner
+end
+
+@inline function cons2entropy(u, equations::CompressibleEulerVectorInvariantEquations3D)
+    rho, v1, v2, v3, rho_theta = u
+
+    return SVector(0, 0, 0, 0, 0, 0)
+end
+
+end # @muladd
---
+++ HandsOn_TrixiAtmo.jl/3_vector_invariant/solver/noncons_kernel_2d.jl
@@ -0,0 +1,275 @@
+using Trixi
+using Trixi:
+	True, False,
+	get_contravariant_vector,
+	multiply_add_to_node_vars!,
+	@threaded,
+	get_surface_node_vars,
+	get_normal_direction,
+	get_node_coords,
+	@turbo,
+	PtrArray,
+	StrideArray
+using Trixi: StaticInt, indices
+@muladd begin
+
+
+@inline function flux_volume_combined_turbo(
+		u_ll,
+		u_rr,
+		orientation_or_normal_direction,
+		equations,
+	)
+		flux_volume_combined(u_ll, u_rr, orientation_or_normal_direction, equations)
+end
+
+end
+
+@inline function Trixi.flux_differencing_kernel!(
+	_du::PtrArray,
+	u_cons::PtrArray,
+	element,
+	mesh::Union{StructuredMesh{2}, UnstructuredMesh2D, P4estMesh{2}},
+	have_nonconservative_terms::True, combine_conservative_and_nonconservative_fluxes::False,
+	equations::CompressibleEulerVectorInvariantEquations2D,
+	volume_flux::typeof(flux_volume_combined_turbo),
+	dg::DGSEM,
+	cache,
+	alpha)
+
+	@unpack derivative_split = dg.basis
+	@unpack contravariant_vectors = cache.elements
+	# Create a temporary array that will be used to store the RHS with permuted
+	# indices `[i, j, v]` to allow using SIMD instructions.
+	# `StrideArray`s with purely static dimensions do not allocate on the heap.
+	du = StrideArray{eltype(u_cons)}(
+		undef,
+		(
+			ntuple(_ -> StaticInt(nnodes(dg)), ndims(mesh))...,
+			StaticInt(nvariables(equations)),
+		),
+	)
+
+	# Convert conserved to primitive variables on the given `element`. In addition
+	# to the usual primitive variables, we also compute logarithms of the density
+	# and pressure to increase the performance of the required logarithmic mean
+	# values.
+	u_prim = StrideArray{eltype(u_cons)}(
+		undef,
+		(
+			ntuple(_ -> StaticInt(nnodes(dg)), ndims(mesh))...,
+			StaticInt(nvariables(equations) + 2),
+		),
+	) # We also compute "+ 2" logs
+
+	@turbo for j in eachnode(dg), i in eachnode(dg)
+		rho = u_cons[1, i, j, element]
+		v1 = u_cons[2, i, j, element]
+		v2 = u_cons[3, i, j, element]
+		rho_theta = u_cons[4, i, j, element]
+		phi = u_cons[5, i, j, element]
+
+		exner = (rho_theta * equations.R / equations.p_0)^(equations.R / equations.c_v)
+		theta = rho_theta / rho
+		u_prim[i, j, 1] = rho
+		u_prim[i, j, 2] = v1
+		u_prim[i, j, 3] = v2
+		u_prim[i, j, 4] = theta
+		u_prim[i, j, 5] = exner
+		u_prim[i, j, 6] = phi
+	end
+
+	# x direction
+	# At first, we create new temporary arrays with permuted memory layout to
+	# allow using SIMD instructions along the first dimension (which is contiguous
+	# in memory).
+	du_permuted = StrideArray{eltype(u_cons)}(
+		undef,
+		(StaticInt(nnodes(dg)), StaticInt(nnodes(dg)), StaticInt(nvariables(equations))),
+	)
+
+	u_prim_permuted = StrideArray{eltype(u_cons)}(
+		undef,
+		(
+			StaticInt(nnodes(dg)),
+			StaticInt(nnodes(dg)),
+			StaticInt(nvariables(equations) + 2),
+		),
+	)
+
+	@turbo for v in indices(u_prim, 3), # v in eachvariable(equations) misses +2 logs
+		j in eachnode(dg),
+		i in eachnode(dg)
+
+		u_prim_permuted[j, i, v] = u_prim[i, j, v]
+	end
+	fill!(du_permuted, zero(eltype(du_permuted)))
+
+	# We must also permute the contravariant vectors.
+	contravariant_vectors_x = StrideArray{eltype(contravariant_vectors)}(
+		undef,
+		(StaticInt(nnodes(dg)), StaticInt(nnodes(dg)), StaticInt(ndims(mesh))),
+	)
+
+	@turbo for j in eachnode(dg), i in eachnode(dg)
+		contravariant_vectors_x[j, i, 1] = contravariant_vectors[1, 1, i, j, element]
+		contravariant_vectors_x[j, i, 2] = contravariant_vectors[2, 1, i, j, element]
+	end
+
+	# Next, we basically inline the volume flux. To allow SIMD vectorization and
+	# still use the symmetry of the volume flux and the derivative matrix, we
+	# loop over the triangular part in an outer loop and use a plain inner loop.
+	for i in eachnode(dg), ii ∈ (i+1):nnodes(dg)
+		@turbo for j in eachnode(dg)
+			rho_ll = u_prim_permuted[j, i, 1]
+			v1_ll = u_prim_permuted[j, i, 2]
+			v2_ll = u_prim_permuted[j, i, 3]
+			theta_ll = u_prim_permuted[j, i, 4]
+			exner_ll = u_prim_permuted[j, i, 5]
+			phi_ll = u_prim_permuted[j, i, 6]
+
+			rho_rr = u_prim_permuted[j, ii, 1]
+			v1_rr = u_prim_permuted[j, ii, 2]
+			v2_rr = u_prim_permuted[j, ii, 3]
+			theta_rr = u_prim_permuted[j, ii, 4]
+			exner_rr = u_prim_permuted[j, ii, 5]
+			phi_rr = u_prim_permuted[j, ii, 6]
+
+			normal_direction_1 =
+				0.5f0 *
+				(contravariant_vectors_x[j, i, 1] + contravariant_vectors_x[j, ii, 1])
+			normal_direction_2 =
+				0.5f0 *
+				(contravariant_vectors_x[j, i, 2] + contravariant_vectors_x[j, ii, 2])
+
+			v_dot_n_ll = v1_ll * normal_direction_1 + v2_ll * normal_direction_2
+			v_dot_n_rr = v1_rr * normal_direction_1 + v2_rr * normal_direction_2
+			v1_avg = 0.5f0 * (v1_ll + v1_rr)
+			v2_avg = 0.5f0 * (v2_ll + v2_rr)
+
+			kin_avg =
+				0.5f0 * (v1_rr * v1_rr + v2_rr * v2_rr + v1_ll * v1_ll + v2_ll * v2_ll)
+			rho_avg = 0.5f0 * (rho_ll + rho_rr)
+			theta_avg = 0.5f0 * (theta_rr + theta_ll)
+			f1 = rho_avg * 0.5f0 * (v_dot_n_ll + v_dot_n_rr)
+			f2 = kin_avg * 0.5f0 * normal_direction_1
+			f3 = kin_avg * 0.5f0 * normal_direction_2
+			f4 = f1 * theta_avg
+
+			jump_v1 = v1_rr - v1_ll
+			jump_v2 = v2_rr - v2_ll
+			gravity = phi_rr - phi_ll
+			vorticity_x =
+				v2_avg * jump_v1 * normal_direction_2 -
+				v2_avg * jump_v2 * normal_direction_1
+			vorticity_y =
+				v1_avg * jump_v2 * normal_direction_1 -
+				v1_avg * jump_v1 * normal_direction_2
+			theta_grad_exner = equations.c_p * theta_avg * (exner_rr - exner_ll)
+			g2 = vorticity_x + (theta_grad_exner + gravity) * normal_direction_1
+			g3 = vorticity_y + (theta_grad_exner + gravity) * normal_direction_2
+
+			# Add scaled fluxes to RHS
+			factor_i = alpha * derivative_split[i, ii]
+			du_permuted[j, i, 1] += factor_i * f1
+			du_permuted[j, i, 2] += factor_i * (f2 + 0.5f0 * g2)
+			du_permuted[j, i, 3] += factor_i * (f3 + 0.5f0 * g3)
+			du_permuted[j, i, 4] += factor_i * f4
+
+			factor_ii = alpha * derivative_split[ii, i]
+			du_permuted[j, ii, 1] += factor_ii * f1
+			du_permuted[j, ii, 2] += factor_ii * (f2 - 0.5f0 * g2)
+			du_permuted[j, ii, 3] += factor_ii * (f3 - 0.5f0 * g3)
+			du_permuted[j, ii, 4] += factor_ii * f4
+		end
+	end
+
+	@turbo for v in eachvariable(equations), j in eachnode(dg), i in eachnode(dg)
+
+		du[i, j, v] = du_permuted[j, i, v]
+	end
+
+	# y direction
+	# We must also permute the contravariant vectors.
+	contravariant_vectors_y = StrideArray{eltype(contravariant_vectors)}(
+		undef,
+		(StaticInt(nnodes(dg)), StaticInt(nnodes(dg)), StaticInt(ndims(mesh))),
+	)
+
+	@turbo for k in eachnode(dg), j in eachnode(dg), i in eachnode(dg)
+		contravariant_vectors_y[i, j, 1] = contravariant_vectors[1, 2, i, j, element]
+		contravariant_vectors_y[i, j, 2] = contravariant_vectors[2, 2, i, j, element]
+	end
+
+	# The memory layout is already optimal for SIMD vectorization in this loop.
+	for j in eachnode(dg), jj ∈ (j+1):nnodes(dg)
+		@turbo for i in eachnode(dg)
+			rho_ll = u_prim[i, j, 1]
+			v1_ll = u_prim[i, j, 2]
+			v2_ll = u_prim[i, j, 3]
+			theta_ll = u_prim[i, j, 4]
+			exner_ll = u_prim[i, j, 5]
+			phi_ll = u_prim[i, j, 6]
+
+			rho_rr = u_prim[i, jj, 1]
+			v1_rr = u_prim[i, jj, 2]
+			v2_rr = u_prim[i, jj, 3]
+			theta_rr = u_prim[i, jj, 4]
+			exner_rr = u_prim[i, jj, 5]
+			phi_rr = u_prim[i, jj, 6]
+
+			normal_direction_1 =
+				0.5f0 *
+				(contravariant_vectors_y[i, j, 1] + contravariant_vectors_y[i, jj, 1])
+			normal_direction_2 =
+				0.5f0 *
+				(contravariant_vectors_y[i, j, 2] + contravariant_vectors_y[i, jj, 2])
+
+			v_dot_n_ll = v1_ll * normal_direction_1 + v2_ll * normal_direction_2
+			v_dot_n_rr = v1_rr * normal_direction_1 + v2_rr * normal_direction_2
+
+			v1_avg = 0.5f0 * (v1_ll + v1_rr)
+			v2_avg = 0.5f0 * (v2_ll + v2_rr)
+			kin_avg =
+				0.5f0 * (v1_rr * v1_rr + v2_rr * v2_rr + v1_ll * v1_ll + v2_ll * v2_ll)
+			rho_avg = 0.5f0 * (rho_ll + rho_rr)
+			theta_avg = 0.5f0 * (theta_rr + theta_ll)
+			f1 = rho_avg * 0.5f0 * (v_dot_n_ll + v_dot_n_rr)
+			f2 = kin_avg * 0.5f0 * normal_direction_1
+			f3 = kin_avg * 0.5f0 * normal_direction_2
+			f4 = f1 * theta_avg
+
+			jump_v1 = v1_rr - v1_ll
+			jump_v2 = v2_rr - v2_ll
+			gravity = phi_rr - phi_ll
+			vorticity_x =
+				v2_avg * jump_v1 * normal_direction_2 -
+				v2_avg * jump_v2 * normal_direction_1
+			vorticity_y =
+				v1_avg * jump_v2 * normal_direction_1 -
+				v1_avg * jump_v1 * normal_direction_2
+			theta_grad_exner = equations.c_p * theta_avg * (exner_rr - exner_ll)
+			g2 = vorticity_x + (theta_grad_exner + gravity) * normal_direction_1
+			g3 = vorticity_y + (theta_grad_exner + gravity) * normal_direction_2
+			# Add scaled fluxes to RHS
+			factor_j = alpha * derivative_split[j, jj]
+			du[i, j, 1] += factor_j * f1
+			du[i, j, 2] += factor_j * (f2 + 0.5f0 * g2)
+			du[i, j, 3] += factor_j * (f3 + 0.5f0 * g3)
+			du[i, j, 4] += factor_j * f4
+
+			factor_jj = alpha * derivative_split[jj, j]
+			du[i, jj, 1] += factor_jj * f1
+			du[i, jj, 2] += factor_jj * (f2 - 0.5f0 * g2)
+			du[i, jj, 3] += factor_jj * (f3 - 0.5f0 * g3)
+			du[i, jj, 4] += factor_jj * f4
+		end
+	end
+
+	# Finally, we add the temporary RHS computed here to the global RHS in the
+	# given `element`.
+	@turbo for v in eachvariable(equations), j in eachnode(dg), i in eachnode(dg)
+
+		_du[v, i, j, element] += du[i, j, v]
+	end
+end
